df9054a9410ebef79dc531b6a09eb825a992caf8
Olajuwon47 <lawal.olajuwonwaheed@gmail.com>
2024-10-13 23:03:42 +0100

    Adding "Add, Edit and Delete" to the project..

--- index.js
+++ index.js
@@ -40,6 +40,7 @@ function updateIframeContent() {
     reference1: document.getElementById("reference-1").value,
     reference2: document.getElementById("reference-2").value,
     reference3: document.getElementById("reference-3").value,
+    addButton:document.querySelector('.js-add-todo-button'),
   };
 
   // Check if any of the input fields or text areas have non-empty values
@@ -162,6 +163,7 @@ function updateIframeContent() {
     iframeBody.innerHTML = "<p>No data entered.</p>";
   }
 }
+
 
 downloadButton.addEventListener("click", () => {
   // Retrieve user input from the main HTML page
@@ -317,3 +319,188 @@ downloadButton.addEventListener("click", () => {
     alert("Please enter some data before generating the resume.");
   }
 });
+
+function addEducation() {
+  const school = document.getElementById('school').value;
+  const degree = document.getElementById('degree').value;
+  const major = document.getElementById('major').value;
+  const graduationDate = document.getElementById('graduation-date').value;
+
+  const educationList = document.getElementById('education-list');
+
+  const listItem = document.createElement('div');
+  listItem.innerHTML = `
+    <div>
+    <input id="school" value="" type="text" name="school" placeholder="School">
+     <input id="degree" value="" type="text" name="degree" placeholder="Degree">
+      <input id="major" value="" type="text" name="major" placeholder="Major">
+      <input id="graduation-date" value="" type="text" name="graduation-date" placeholder="Graduation Date">
+     </div>
+      <button onclick="editEducation(this)">
+       <i class="fa-light fa-pen-to-square fa-lg" style="color: #0a0a0a;"></i>
+      </button>
+      <button onclick="deleteEducation(this)">
+       <i class="fa-thin fa-trash fa-lg" style="color: #0d0d0d;"></i>
+      </button>
+    
+  `;
+
+  educationList.appendChild(listItem);
+
+  // Clear the input fields after adding
+  document.getElementById('school').value = '';
+  document.getElementById('degree').value = '';
+  document.getElementById('major').value = '';
+  document.getElementById('graduation-date').value = '';
+}
+
+function editEducation(button) {
+  const listItem = button.parentElement;
+  const fields = listItem.innerText.split(',');
+  document.getElementById('school').value = fields[0];
+  document.getElementById('degree').value = fields[1];
+  document.getElementById('major').value = fields[2];
+  document.getElementById('graduation-date').value = fields[3].replace(' Edit Delete', '');
+
+  listItem.remove();
+}
+
+function deleteEducation(button) {
+  button.parentElement.remove();
+}
+
+// Repeat similar corrections for addWorkExperience, addSkill, and addReference:
+
+function addWorkExperience() {
+  const company = document.getElementById('company').value;
+  const title = document.getElementById('title').value;
+  const startDate = document.getElementById('start-date').value;
+  const endDate = document.getElementById('end-date').value;
+
+  const workExperienceList = document.getElementById('work-experience-list');
+
+  const listItem = document.createElement('div');
+  listItem.innerHTML = `
+    <div>
+     <input id="company" value="" type="text" name="company" placeholder="Company">
+      <input id="title" value="" type="text" name="title" placeholder="Title">
+      <input id="start-date" value="" type="text" name="start-date" placeholder="Start Date">
+      <input id="end-date" value="" type="text" name="end-date" placeholder="End Date">
+      </div>
+      <button onclick="editWorkExperience(this)">
+        <i class="fa-light fa-pen-to-square fa-lg" style="color: #0a0a0a;"></i>
+      </button>
+      <button onclick="deleteWorkExperience(this)">
+       <i class="fa-thin fa-trash fa-lg" style="color: #0d0d0d;"></i>
+      </button>
+    
+  `;
+
+  workExperienceList.appendChild(listItem);
+
+  document.getElementById('company').value = '';
+  document.getElementById('title').value = '';
+  document.getElementById('start-date').value = '';
+  document.getElementById('end-date').value = '';
+}
+
+function editWorkExperience(button) {
+  const listItem = button.parentElement;
+  const fields = listItem.innerText.split(',');
+  document.getElementById('company').value = fields[0];
+  document.getElementById('title').value = fields[1];
+  document.getElementById('start-date').value = fields[2];
+  document.getElementById('end-date').value = fields[3].replace(' Edit Delete', '');
+
+  listItem.remove();
+}
+
+function deleteWorkExperience(button) {
+  button.parentElement.remove();
+}
+
+function addSkill() {
+  const skill1 = document.getElementById("skill-1").value;
+  const skill2 = document.getElementById("skill-2").value;
+  const skill3 = document.getElementById("skill-3").value;
+
+  const skillsList = document.getElementById('skills-list');
+
+  const listItem = document.createElement('div');
+  listItem.innerHTML = `
+    <div>
+    <input id="skill-1" vaule="" type="text" name="skill-1" placeholder="Skill 1">
+      <input id="skill-2" vaule="" type="text" name="skill-2" placeholder="Skill 2">
+      <input id="skill-3" vaule="" type="text" name="skill-3" placeholder="Skill 3">
+     </div>
+      <button onclick="editSkills(this)">
+       <i class="fa-light fa-pen-to-square fa-lg" style="color: #0a0a0a;"></i>
+      </button>
+      <button onclick="deleteSkills(this)">
+        <i class="fa-thin fa-trash fa-lg" style="color: #0d0d0d;"></i>
+      </button>
+  `;
+
+  skillsList.appendChild(listItem);
+
+  document.getElementById('skill-1').value = '';
+  document.getElementById('skill-2').value = '';
+  document.getElementById('skill-3').value = '';
+}
+
+function editSkills(button) {
+  const listItem = button.parentElement;
+  const fields = listItem.innerText.split(',');
+  document.getElementById('skill-1').value = fields[0];
+  document.getElementById('skill-2').value = fields[1];
+  document.getElementById('skill-3').value = fields[2].replace(' Edit Delete', '');
+
+  listItem.remove();
+}
+
+function deleteSkills(button) {
+  button.parentElement.remove();
+}
+
+function addReference() {
+  const reference1 = document.getElementById("reference-1").value;
+  const reference2 = document.getElementById("reference-2").value;
+  const reference3 = document.getElementById("reference-3").value;
+
+  const referencesList = document.getElementById('references-list');
+
+  const listItem = document.createElement('div');
+  listItem.innerHTML = `
+    <div>
+    <input id="reference-1" value="" type="text" name="reference-1" placeholder="Reference 1">
+      <input id="reference-2" value="" type="text" name="reference-2" placeholder="Reference 2">
+      <input id="reference-3" value="" type="text" name="reference-3" placeholder="Reference 3">
+      </div>
+      <button onclick="editReference(this)">
+       <i class="fa-light fa-pen-to-square fa-lg" style="color: #0a0a0a;"></i>
+      </button>
+      <button onclick="deleteReference(this)">
+       <i class="fa-thin fa-trash fa-lg" style="color: #0d0d0d;"></i>
+      </button>
+  `;
+
+  referencesList.appendChild(listItem);
+
+  document.getElementById('reference-1').value = '';
+  document.getElementById('reference-2').value = '';
+  document.getElementById('reference-3').value = '';
+}
+
+function editReference(button) {
+  const listItem = button.parentElement;
+  const fields = listItem.innerText.split(',');
+  document.getElementById('reference-1').value = fields[0];
+  document.getElementById('reference-2').value = fields[1];
+  document.getElementById('reference-3').value = fields[2].replace(' Edit Delete', '');
+
+  listItem.remove();
+}
+
+function deleteReference(button) {
+  button.parentElement.remove();
+}
